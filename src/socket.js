924c7f6fe022d7069186ff76c015596d46636665
rajan8 <rajan@auberginesolutions.com>
2023-07-28 00:24:54 +0530

    fix socket

--- src/socket.js
+++ src/socket.js
@@ -1,12 +1,12 @@
 import { io } from "socket.io-client";
 
 
-// "undefined" means the URL will be computed from the `window.location` object
-const URL =
-  process.env.NODE_ENV === "production"
-    ? undefined
-    : process.env.REACT_APP_BASE_URI;
+// // "undefined" means the URL will be computed from the `window.location` object
+// const URL =
+//   process.env.NODE_ENV === "production"
+//     ? undefined
+//     : process.env.REACT_APP_BASE_URI;
 
-export const socket = io(URL, {
-  autoConnect: false
+export const socket = io(process.env.REACT_APP_BASE_URI, {
+  transports: ['websocket']
 });
